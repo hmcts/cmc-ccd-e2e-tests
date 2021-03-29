8f2cc715a9303ba18231fe1aadd5b25b7d68a5b3
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-03-29 09:24:39 +0100

    remove proxy

--- src/test/end-to-end/ccdApi/restHelper.js
+++ src/test/end-to-end/ccdApi/restHelper.js
@@ -1,25 +1,11 @@
-const config = require('../../config.js');
-
 const fetch = require('node-fetch');
-const HttpProxyAgent = require('http-proxy-agent');
-const HttpsProxyAgent = require('https-proxy-agent');
 
 const {retry} = require('./retryHelper');
-
-const PROXY_SERVER = `http://${config.proxyServer}`;
-const PROXY_AGENT = url => {
-    return config.proxyServer ? getProxyServer(url) : null;
-};
-
-const getProxyServer = (url) => {
-    return url.startsWith('http:') ? new HttpProxyAgent(PROXY_SERVER) : new HttpsProxyAgent(PROXY_SERVER);
-};
 
 const request = (url, headers, body, method = 'POST') => fetch(url, {
     method: method,
     body: body ? JSON.stringify(body) : undefined,
-    headers: headers,
-    agent: PROXY_AGENT(url)
+    headers: headers
 });
 
 const retriedRequest = async (url, headers, body, method = 'POST', expectedStatus = 200) => {
