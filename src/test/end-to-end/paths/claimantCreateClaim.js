47df588668e4b0fa703ffb7a86d55d4999bfd1a9
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-04-09 10:31:22 +0100

    fix lint issues

--- src/test/end-to-end/paths/claimantCreateClaim.js
+++ src/test/end-to-end/paths/claimantCreateClaim.js
@@ -1,13 +1,18 @@
+'use strict';
+
+const testConfig = require('../../config');
 const {userType} = require('../common/Constants');
 
 Feature('Create claim flow)');
 
- Scenario('Create Claimant flow', async ({I}) => {
+const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
+
+Scenario('Create Claimant flow', async ({I}) => {
     //claimant steps
     await I.amOnCitizenAppPage('');
     await I.authenticateWithIdam(userType.CITIZEN, true);
     const claimRef = await I.createClaim();
-    console.log('Create claim ref..', claimRef);
+    logger.info({message: 'Claimant has created a claim with reference..', claimRef});
     await I.click('Sign out');
- }).tag('@crossbrowser')
-      .retry(testConfig.TestRetryScenarios);
+}).tag('@crossbrowser')
+    .retry(testConfig.TestRetryScenarios);
