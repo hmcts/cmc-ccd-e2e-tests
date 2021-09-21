31bdb7a11c6fcffe0608da866fe8f2e2f7ba7d01
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2021-09-21 13:55:38 +0100

    Update Jenkinsfile_nightly (#16)

--- src/test/config.js
+++ src/test/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     TestEndToEndUrl: process.env.TEST_E2E_URL || 'https://manage-case.aat.platform.hmcts.net/',
-    TestFrontEndUrl: process.env.TEST_FRONT_END_URL || 'https://moneyclaims.aat.platform.hmcts.net//',
+    TestFrontEndUrl: process.env.TEST_FRONT_END_URL || 'https://moneyclaims.aat.platform.hmcts.net/',
     TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
     TestRetryFeatures: process.env.RETRY_FEATURES || 0,
     TestRetryScenarios: process.env.RETRY_SCENARIOS || 2,
