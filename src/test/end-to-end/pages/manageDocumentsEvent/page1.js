b897f49adc84298d431fff522f0e319df9e941a4
jneelakandan <91900624+jneelakandan@users.noreply.github.com>
2021-11-11 10:00:34 +0000

    Update page1.js

--- src/test/end-to-end/pages/manageDocumentsEvent/page1.js
+++ src/test/end-to-end/pages/manageDocumentsEvent/page1.js
@@ -3,11 +3,9 @@
 const testConfig = require('../../../config');
 const manageDocumentsConfig = require('./manageDocumentsConfig');
 const commonConfig = require('../common/commonConfig');
-const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
 
 module.exports = async function (formType = 'Other', docSubmissionFieldEnabled = false) {
     const I = this;
-    logger.info({message: 'Inside ManagedDocument'});
     await I.waitInUrl('ManageDocuments/ManageDocuments1', testConfig.TestTimeToWaitForText);
     
     await I.waitForElement('#staffUploadedDocuments', testConfig.TestTimeToWaitForText);
