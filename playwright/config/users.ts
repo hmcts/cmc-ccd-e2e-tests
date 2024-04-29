e3308ee3e3b469626e2bdcf5d30d3a7058d788bb
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-04-29 16:02:49 +0100

    integrate-playwright

--- playwright/config/users.ts
+++ playwright/config/users.ts
@@ -4,24 +4,24 @@ import UserRole from "../models/UserRole";
 import UserType from "../models/UserType";
 
 export const legalAdvisor: User = {
-  email: process.env.LA_USER_EMAIL || "civilmoneyclaims+la@gmail.com",
-  password: process.env.LA_USER_PASSWORD || "Password12",
+  email: process.env.LA_USER_EMAIL,
+  password: process.env.LA_USER_PASSWORD,
   type: UserType.LEGALADVISOR,
   role: UserRole.CASEWORKER,
   cookiesPath: "playwright/fixtures/.cookies/legal-advisor.json",
 };
 
 export const caseworker: User = {
-  email: process.env.CW_USER_EMAIL || "civilmoneyclaims+ccda@gmail.com",
-  password: process.env.CW_USER_PASSWORD || "Password12",
+  email: process.env.CW_USER_EMAIL,
+  password: process.env.CW_USER_PASSWORD,
   type: UserType.CASEWORKER,
   role: UserRole.CASEWORKER,
   cookiesPath: "playwright/fixtures/.cookies/caseworker.json",
 };
 
 export const judge: User = {
-  email: process.env.JUDGE_USER_EMAIL || "civilmoneyclaims+judge@gmail.com",
-  password: process.env.JUDGE_USER_PASSWORD || "Password12",
+  email: process.env.JUDGE_USER_EMAIL,
+  password: process.env.JUDGE_USER_PASSWORD,
   type: UserType.JUDGE,
   role: UserRole.CASEWORKER,
   cookiesPath: "playwright/fixtures/.cookies/judge.json",
