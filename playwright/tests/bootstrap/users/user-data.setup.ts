f89b19074e906159bb5018f505a0a8b70c816116
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-07-18 10:42:16 +0100

    Fix lint issues

--- playwright/tests/bootstrap/users/user-data.setup.ts
+++ playwright/tests/bootstrap/users/user-data.setup.ts
@@ -2,7 +2,7 @@ import config from '../../../config/config';
 import { caseworker, claimants, defendants, judge, legalAdvisor } from '../../../config/users';
 import { test as setup } from '../../../playwright-fixtures/index';
 
-setup.describe(`Setting up user data`, () => {
+setup.describe('Setting up user data', () => {
   setup.describe.configure({mode: 'parallel'});
   if(config.skipCitizenSetup) {
     setup('Claimant(s)', async ({ApiUsersSteps}) => {
