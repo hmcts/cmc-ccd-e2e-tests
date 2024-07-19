f45a02fd63dbb9f63c55c0ac61148d6a5720160e
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-07-19 13:55:38 +0100

    Removing set up test

--- playwright/tests/bootstrap/users/citizen-users.setup.ts
+++ playwright/tests/bootstrap/users/citizen-users.setup.ts
@@ -7,11 +7,9 @@ if(!config.skipCitizenSetup) {
     setup.describe.configure({mode: 'parallel'});
     setup('Claimant(s)', async ({ApiUsersSteps: ApiCitizenUsersSteps}) => {
       await ApiCitizenUsersSteps.CreateCitizenUsers(claimants);
-      throw new Error();
     });
     setup('Defendant(s)', async ({ApiUsersSteps: ApiCitizenUsersSteps}) => {
       await ApiCitizenUsersSteps.CreateCitizenUsers(defendants);
-      throw new Error();
     });
   });
 }
