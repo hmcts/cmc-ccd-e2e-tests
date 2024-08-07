aedbf0e8bb021d8bef370427ab2a9016a07a643b
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-08-07 14:19:26 +0100

    update test configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         ],
       ]
     : 'list',
-  timeout: 8 * 30 * 1000,
+  timeout: 360_000,
   expect: {
     timeout: 30_000,
     toPass: {
@@ -76,7 +76,6 @@ export default defineConfig({
       name: 'safari-full-functional',
       use: { ...devices['Desktop Safari'] },
       dependencies: ['users-auth-setup'],
-      timeout: 360_000,
     },
   ],
 });
