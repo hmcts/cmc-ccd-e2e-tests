d7a090c494525c494fd8479f44dc64f63bb03455
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-12-04 09:31:24 +0000

    freeMediation fix

--- playwright/pages/ocmc/response/response/response-dashboard/response-dashboard-content.ts
+++ playwright/pages/ocmc/response/response/response-dashboard/response-dashboard-content.ts
@@ -1,8 +1,8 @@
-export const getLinks = (reference: string) => ({
+export const links = {
   freeMediation: {
     title: 'Free telephone mediation',
   },
   hearingDetails: {
     title: `Give us details in case there’s a hearing `,
   },
-});
+};
