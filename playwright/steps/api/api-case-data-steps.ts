5a226b41b1079f2c7470c4b50a8ef8b4eb1a8062
Ruban <ruban72@hotmail.com>
2026-03-16 15:30:58 +0000

    DTSCCI-3095: Fix e2e tests

--- playwright/steps/api/api-case-data-steps.ts
+++ playwright/steps/api/api-case-data-steps.ts
@@ -3,6 +3,7 @@ import RequestsFactory from '../../requests/requests-factory';
 import TestData from '../../models/test-data';
 import { judge } from '../../config/users';
 import BaseApiSteps from '../../base/base-api-steps';
+import CCDCaseData from '../../models/case-data/ccd-case-data';
 
 @AllMethodsStep()
 export default class ApiCaseDataSteps extends BaseApiSteps {
@@ -14,7 +15,57 @@ export default class ApiCaseDataSteps extends BaseApiSteps {
     await new Promise((resolve) => setTimeout(resolve, ms));
   }
 
+  private populateClaimFromCcdData(ccdCaseData: CCDCaseData) {
+    const applicant = ccdCaseData.applicants?.[0]?.value;
+    const respondent = ccdCaseData.respondents?.[0]?.value;
+    const respondentDetail = respondent?.claimantProvidedDetail;
+    this.claimStoreCaseData.claim = {
+      externalId: ccdCaseData.externalId,
+      claimants: [
+        {
+          type: applicant?.partyDetail?.type,
+          id: applicant?.partyDetail?.idamId,
+          name: applicant?.partyName,
+          address: {
+            line1: applicant?.partyDetail?.primaryAddress?.AddressLine1,
+            line2: applicant?.partyDetail?.primaryAddress?.AddressLine2,
+            line3: applicant?.partyDetail?.primaryAddress?.AddressLine3,
+            city: applicant?.partyDetail?.primaryAddress?.PostTown,
+            postcode: applicant?.partyDetail?.primaryAddress?.PostCode,
+          },
+        },
+      ],
+      defendants: [
+        {
+          type: respondentDetail?.type,
+          id: respondent?.letterHolderId,
+          name: respondent?.claimantProvidedPartyName,
+          address: {
+            line1: respondentDetail?.primaryAddress?.AddressLine1,
+            line2: respondentDetail?.primaryAddress?.AddressLine2,
+            line3: respondentDetail?.primaryAddress?.AddressLine3,
+            city: respondentDetail?.primaryAddress?.PostTown,
+            postcode: respondentDetail?.primaryAddress?.PostCode,
+          },
+          title: respondentDetail?.title,
+          firstName: respondentDetail?.firstName,
+          lastName: respondentDetail?.lastName,
+          email: respondentDetail?.emailAddress,
+        },
+      ],
+      payment: undefined,
+      amount: undefined,
+      feeAmountInPennies: parseInt(ccdCaseData.feeAmountInPennies) || 0,
+      timeline: undefined,
+      reason: ccdCaseData.reason,
+    };
+  }
+
   async FetchClaimStoreCaseData() {
+    const { ccdRequests } = this.requestsFactory;
+    const ccdCaseData = await ccdRequests.fetchCcdCaseData(this.claimStoreCaseData.id, judge);
+    this.populateClaimFromCcdData(ccdCaseData);
+    this.setCcdCaseData = ccdCaseData;
     console.log('Claim store case data fetched successfully');
   }
 
@@ -24,6 +75,10 @@ export default class ApiCaseDataSteps extends BaseApiSteps {
       this.claimStoreCaseData.id,
       judge,
     );
+    const ccdCaseData = await ccdRequests.fetchCcdCaseData(this.claimStoreCaseData.id, judge);
+    this.populateClaimFromCcdData(ccdCaseData);
+    this.setCcdCaseData = ccdCaseData;
+    console.log('Claim store case data with letter id fetched successfully');
   }
 
   async FetchClaimSecurityPin() {
